b3d9819cce5b130f6eddd5d4cd2e2a3b42be198a
maxcrn <maxcrn@icloud.com>
2020-03-25 15:37:17 +0100

    Tri avec InstaLoader

--- main.py
+++ main.py
@@ -41,6 +41,26 @@ for post in posts:
             if post.location.name == "La Rochelle, France":
                 print("Passage dans Google Vision")
         else:
-            print("Passage dans Google Vision")
+            detect_landmarks_uri(post.url)
 
 
+    def detect_landmarks_uri(uri):
+        """Detects landmarks in the file located in Google Cloud Storage or on the
+        Web."""
+        from google.cloud import vision
+        client = vision.ImageAnnotatorClient()
+        image = vision.types.Image()
+        image.source.image_uri = uri
+
+        response = client.landmark_detection(image=image)
+        landmarks = response.landmark_annotations
+        print('Landmarks:')
+
+        for landmark in landmarks:
+            print(landmark.description)
+
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
