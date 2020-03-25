341ddd7f372088ada1d90c1e92f006f5a7f89031
maxcrn <maxcrn@icloud.com>
2020-03-25 15:02:26 +0100

    Tri avec InstaLoader

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from itertools import dropwhile, takewhile
 
 import instaloader
 
-# Login sur Instagram avec identifiant et mot de passe
-L.login("instatrackeruppa", "instatracker")
+# Données en longitude et latitude de La Rochelle
+lngLRMin = -1.234431
+lngLRMax = -1.110850
+latLRMin = 46.133955
+latLRMax = 46.190103
 
 # Pour que le json soit bien formaté et non compressé
 L = instaloader.Instaloader(compress_json=False)
 
+# Login sur Instagram avec identifiant et mot de passe
+L.login("instatrackeruppa", "instatracker")
 ## Pour télécharger un certain nombre de photos sur un hashtag ##
 # L.download_hashtag('larochelletourisme', max_count=10)
 
@@ -26,6 +31,16 @@ FIN = datetime(2020, 2, 25)
 DEBUT = datetime(2020, 2, 21)
 
 for post in posts:
-    if FIN >= post.date >= DEBUT:
+    if FIN >= post.date >= DEBUT: # Condition sur la date du post
         print(post.date)
-        L.download_post(post, '#larochelletourisme')
+        # Condition sur la localisation du post
+        if post.location != None and lngLRMin < post.location.lng < lngLRMax and latLRMin < post.location.lat < latLRMax:
+            print(post.location)
+            L.download_post(post, '#larochelletourisme')
+            # Condition sur le nom de la localisation (si pas assez précis, envoi à Google Vision)
+            if post.location.name == "La Rochelle, France":
+                print("Passage dans Google Vision")
+        else:
+            print("Passage dans Google Vision")
+
+
