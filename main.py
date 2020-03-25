5b739ff4354f72612255db3e062a0a5dafe49517
maxcrn <maxcrn@icloud.com>
2020-03-25 16:54:03 +0100

    Suppression du donwload post qui est devenu inutile

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from itertools import dropwhile, takewhile
 
 import instaloader
 
-os.environ[
-    "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/maxcarin/Documents/Cours/M1 S2/Gestion de Projet/InstaTracker-d209c2a2aab7.json"
+# Credentials pour le login sur Google Vision
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/maxcarin/Documents/Cours/M1 S2/Gestion de Projet/InstaTracker-d209c2a2aab7.json"
 
 # Données en longitude et latitude de La Rochelle
 lngLRMin = -1.234431
@@ -13,11 +13,11 @@ lngLRMax = -1.110850
 latLRMin = 46.133955
 latLRMax = 46.190103
 
-# Pour que le json soit bien formaté et non compressé
-L = instaloader.Instaloader(compress_json=False)
+L = instaloader.Instaloader()
 
 # Login sur Instagram avec identifiant et mot de passe
 L.login("instatrackeruppa", "instatracker")
+
 ## Pour télécharger un certain nombre de photos sur un hashtag ##
 # L.download_hashtag('larochelletourisme', max_count=10)
 
@@ -31,16 +31,18 @@ posts = L.get_hashtag_posts('larochelletourisme')
 # posts = instaloader.Profile.from_username(L.context, PROFILE).get_posts()
 
 # Pour télécharger des photos entre deux dates sur un hashtag
-FIN = datetime(2020, 2, 25)
-DEBUT = datetime(2020, 2, 21)
+FIN = datetime(2020, 3, 24)
+DEBUT = datetime(2020, 3, 21)
 
 for post in posts:
-    if FIN >= post.date >= DEBUT:  # Condition sur la date du post
+    # Condition sur la date du post
+    if FIN >= post.date >= DEBUT:
         print(post.date)
+
         # Condition sur la localisation du post
         if post.location != None and lngLRMin < post.location.lng < lngLRMax and latLRMin < post.location.lat < latLRMax:
             print(post.location)
-            #L.download_post(post, '#larochelletourisme')
+
             # Condition sur le nom de la localisation (si pas assez précis, envoi à Google Vision)
             if post.location.name == "La Rochelle, France":
                 detect_landmarks_uri(post.url)  # Analyse par Google Vision de la photo via son url
@@ -57,12 +59,21 @@ for post in posts:
 
         response = client.landmark_detection(image=image)
         landmarks = response.landmark_annotations
-        print('Landmarks:')
+        if not(landmarks):
+            print('Pas de landmark')
+        else:
+            print("Landmark(s) :")
 
         for landmark in landmarks:
-            print(landmark.description) # Affichage du nom du hotspot
-            print(landmark.locations[0].lat_lng.latitude) # Affichage de la longitude du hotspot
-            print(landmark.locations[0].lat_lng.longitude) # Affichage de la latitude du hotspot
+
+            # Condition sur la longitude et latitude du landmark
+            if lngLRMin < landmark.locations[0].lat_lng.longitude < lngLRMax \
+                    and latLRMin < landmark.locations[0].lat_lng.latitude < latLRMax:
+                print(landmark.description)  # Affichage du nom du hotspot
+                print(landmark.locations[0].lat_lng.latitude) # Affichage de la longitude du hotspot
+                print(landmark.locations[0].lat_lng.longitude) # Affichage de la latitude du hotspot
+            else:
+                print("Localisation hors La Rochelle")
 
         if response.error.message:
             raise Exception(
