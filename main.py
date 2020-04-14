26bd6b99619eab7b4260cd625d710d39948a9923
maxcrn <maxcrn@icloud.com>
2020-04-14 18:46:08 +0200

    Ajout de la correction du tableau de hotspots Ajout de la trace Ajout de la correction de la trace

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ L.login("instatrackeruppa", "instatracker")
 ## Pour télécharger des photos entre deux dates sur un hashtag ou un profil ##
 
 # Spécification du hastag sur lequel télécharger les photos
-#posts = L.get_hashtag_posts('larochelletourisme')
 posts = L.get_hashtag_posts('larochelletourisme')
+#posts = L.get_hashtag_posts('toursaintnicolas')
 
 # Spécification du profil sur lequel télécharger les photos
 # posts = instaloader.Profile.from_username(L.context, PROFILE).get_posts()
@@ -41,12 +41,50 @@ resInstaLoader = False
 resGVision = False
 i = 1
 
+
 # Compteur de hotspots
 compteurHSIL = {}
 compteurHSGV = {}
+hotspots = {}
+
+# Suivi de trace
+traces = {}
 
 # Booléen pour l'analyse de la trace
 traceEnCours = False
+
+
+def correctionTrace(traceTableau):
+    tableauCorrige = []
+    for i in (traceTableau):
+        if "Lanterne" in i or "lanterne" in i:
+            tableauCorrige.append("Tour de la Lanterne")
+
+        elif "Horloge" in i or "horloge" in i:
+            tableauCorrige.append("La Grosse Horloge")
+
+        elif "Nicolas" in i or "nicolas" in i:
+            tableauCorrige.append("Tour St-Nicolas")
+
+        elif "Port" in i or "port" in i or "Tour" in i or "tour" in i or "Harbour" in i or "harbour" in i \
+                or "Towers" in i or "towers" in i:
+            tableauCorrige.append("Port de La Rochelle")
+
+        elif "Aquarium" in i or "aquarium" in i:
+            tableauCorrige.append("Aquarium de La Rochelle")
+
+        elif "Gabut" in i or "gabut" in i:
+            tableauCorrige.append("Le Gabut")
+
+        elif "Minimes" in i or "minimes" in i:
+            tableauCorrige.append("Les Minimes")
+
+        else:
+            tableauCorrige.append(i)
+
+    return tableauCorrige
+
+
 
 for post in posts:
     # Condition sur la date du post
@@ -92,6 +130,7 @@ for post in posts:
             utilActuel = post.owner_username
             postsUtilActuel = instaloader.Profile.from_username(L.context, utilActuel).get_posts()
             traceEnCours = True
+            traceTemp = []
             for postUtilActuel in postsUtilActuel:
 
                 # Si la date d'un post est à J-2 ou J+2 de la date du post de base
@@ -105,6 +144,7 @@ for post in posts:
                             and lngLRMin < postUtilActuel.location.lng < lngLRMax \
                             and latLRMin < postUtilActuel.location.lat < latLRMax:
                         print("Analyse InstaLoader : " + postUtilActuel.location.name)
+                        traceTemp.append(postUtilActuel.location.name)
 
                         # Condition sur le nom de la localisation (si pas assez précis, envoi à Google Vision)
                         if postUtilActuel.location.name == "La Rochelle, France":
@@ -118,15 +158,19 @@ for post in posts:
                         detect_landmarks_uri(postUtilActuel.url)
 
                     j = j + 1
-
+            traceTempClean = correctionTrace(traceTemp)
+            traces[post.owner_username] = traceTempClean
             # On remet les varaibles de test à False
             resInstaLoader = False
             resGVision = False
             traceEnCours = False
             print("Fin de l'analyse de la trace utilisateur avec " + str(j) + " photos analysées")
 
+        print(traces)
         print("")
         i = i + 1
+
+
 
 
     # Fonction pour la detection des landmarks via Google Vision
@@ -157,6 +201,8 @@ for post in posts:
                         compteurHSGV[landmark.description] += 1
                     else:
                         compteurHSGV[landmark.description] = 1
+                else:
+                    traceTemp.append(landmark.description)
                 return True
             else:
                 print("Localisation hors La Rochelle")
@@ -170,3 +216,59 @@ for post in posts:
                 'https://cloud.google.com/apis/design/errors'.format(
                     response.error.message))
 
+
+def creationCompteur(compteur):
+    # key : values
+    for i in (compteur):
+        if "Lanterne" in i or "lanterne" in i:
+            if "Tour de la Lanterne" in hotspots:
+                hotspots["Tour de la Lanterne"] += compteur[i]
+            else:
+                hotspots["Tour de la Lanterne"] = compteur[i]
+        elif "Horloge" in i or "horloge" in i:
+            if "La Grosse Horloge" in hotspots:
+                hotspots["La Grosse Horloge"] += compteur[i]
+            else:
+                hotspots["La Grosse Horloge"] = compteur[i]
+
+        elif "Nicolas" in i or "nicolas" in i:
+            if "Tour St-Nicolas" in hotspots:
+                hotspots["Tour St-Nicolas"] += compteur[i]
+            else:
+                hotspots["Tour St-Nicolas"] = compteur[i]
+
+        elif "Port" in i or "port" in i or "Tour" in i or "tour" in i or "Harbour" in i or "harbour" in i \
+                or "Towers" in i or "towers" in i:
+            if "Port de La Rochelle" in hotspots:
+                hotspots["Port de La Rochelle"] += compteur[i]
+            else:
+                hotspots["Port de La Rochelle"] = compteur[i]
+
+        elif "Aquarium" in i or "aquarium" in i:
+            if "Aquarium de La Rochelle" in hotspots:
+                hotspots["Aquarium de La Rochelle"] += compteur[i]
+            else:
+                hotspots["Aquarium de La Rochelle"] = compteur[i]
+
+        elif "Gabut" in i or "gabut" in i:
+            if "Le Gabut" in hotspots:
+                hotspots["Le Gabut"] += compteur[i]
+            else:
+                hotspots["Le Gabut"] = compteur[i]
+
+        elif "Minimes" in i or "minimes" in i:
+            if "Les Minimes" in hotspots:
+                hotspots["Les Minimes"] += compteur[i]
+            else:
+                hotspots["Les Minimes"] = compteur[i]
+
+        else:
+            if i in hotspots:
+                hotspots[i] += compteur[i]
+            else:
+                hotspots[i] = compteur[i]
+
+
+creationCompteur(compteurHSIL)
+creationCompteur(compteurHSGV)
+print(hotspots)
